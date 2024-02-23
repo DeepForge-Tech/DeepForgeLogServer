c96fa051b26d22f5f65d39e6904cc52b01944f36
logserver <logserver@ssh1.paris1.alwaysdata.com>
2024-02-23 15:18:52 +0100

    Refactoring

--- StartProject.sh
+++ StartProject.sh
@@ -1,5 +1,5 @@
-../redis/src/redis-server --daemonize yes
-cd ./backend/users && npm install && nohup npm run dev< /dev/null &
-cd ./backend/logs && npm install && nohup npm run dev < /dev/null &
-cd ./backend/database && npm install && nohup npm run dev < /dev/null &
+#../redis/src/redis-server --daemonize yes
+cd ./backend/users && nohup npm run dev < /dev/null &
+cd ./backend/logs && nohup npm run dev < /dev/null &
+cd ./backend/database && nohup npm run dev < /dev/null &
 cd ./frontend/ && npm run dev
