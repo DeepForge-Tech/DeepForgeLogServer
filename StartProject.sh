bfa6cf66e9b0245eb269a78a2871feccc98a768b
blackflame576 <pws15548@gmail.com>
2024-02-24 23:42:26 +0300

    Refactoring

--- StartProject.sh
+++ StartProject.sh
@@ -1,5 +1,5 @@
 #../redis/src/redis-server --daemonize yes
-cd ./backend/users && nohup npm run dev < /dev/null &
-cd ./backend/logs && nohup npm run dev < /dev/null &
-cd ./backend/database && nohup npm run dev < /dev/null &
+cd ./backend/users && nohup npm run production < /dev/null &
+cd ./backend/logs && nohup npm run production < /dev/null &
+cd ./backend/database && nohup npm run production < /dev/null &
 cd ./frontend/gateway && npm run production
